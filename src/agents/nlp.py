e5bd0ca2a18344c3ba643d888c151382e970a162
Alex Chin <alexanderchin@college.harvard.edu>
2022-03-15 17:44:10 -0400

    debug

--- src/agents/nlp.py
+++ src/agents/nlp.py
@@ -202,6 +202,11 @@ class BaseNLPMetaAgent(BaseAgent):
                 break
 
             # increase the number of ways on a patience-based system
+            print(f"self.wi_mode; {self.wi_mode}")
+            print(f"self.iter_with_no_improv_since_wi: {self.iter_with_no_improv_since_wi}")
+            print(f"self.config.dataset.train.ways_patience: {self.config.dataset.train.ways_patience}")
+            print(f"self.config.dataset.train.n_ways: {self.config.dataset.train.n_ways}")
+            print(f"self.config.dataset.train.max_ways: {self.config.dataset.train.max_ways}")
             if self.wi_mode == "patience":
                 if self.iter_with_no_improv_since_wi > self.config.dataset.train.ways_patience:
                     self.iter_with_no_improv_since_wi = 0
